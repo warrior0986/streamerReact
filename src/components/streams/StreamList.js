f0923c0ad729fb7f1b3ff63f8002f7cda55dd29b
warrior0986 <diegoguerrero0986@gmail.com>
2019-05-16 21:45:56 -0500

    252 Linking to Stream Creation

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,74 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-    return <div>StreamList</div>
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderAdmin(stream) {
+        if(stream.userId === this.props.currentUserId) {
+            return(
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            );
+        }
+    }
+
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    renderCreate() {
+        if(this.props.signedIn) {
+            return(
+                <div style={{textAlign : 'right'}}>
+                    <Link className="ui button primary" to="/streams/new">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+                {this.renderCreate()}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => { //toma lo del redux store y lo pasa como props a los componentes
+    return { 
+        streams: Object.values(state.streams), //extraigo cada stream sin el id puesto en el streamReducer con la _.mapKeys
+        currentUserId: state.auth.userId,
+        signedIn: state.auth.isSignedIn
+    }; 
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
